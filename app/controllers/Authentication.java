a377355066f0abfb33e073e9878eb4c7ba700b00
User <User@H27-NotePC04>
2015-05-22 17:07:18 +0900

    log-in画面製作中

---
+++ app/controllers/Authentication.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.*;
+
+public class Authentication extends Controller {
+    public static Result index() {
+        return TODO;
+    }
+
+    public static Result authenticate() {
+        return TODO;
+    }
+}
