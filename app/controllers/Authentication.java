64f8e7af9bf23aaaa42af6938dabcb51d7c6e008
User <User@H27-NotePC04>
2015-05-25 11:43:09 +0900

    login画面編集中

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -11,17 +11,38 @@ public class Authentication extends Controller {
 	 public static class Login {
 	         public String username;
 	         public String password;
+
+	 public String validate() {
+	        if (authenticate(username, password) == null) {
+	        return "Invalid username and password";
+	        	}
+	        return null;
+	 	    }
 	 }
 
-	 public static Form<Login> loginForm = Form.form(Login.class);
+	 public static Result login() {
+		    return ok(
+		        login.render(form(Login.class))
+		    );
+		}
 
-    public static Result index() {
-    	return ok(index.render(loginForm));
-    }
+	 public static Result authenticate() {
+		    Form loginForm = form(Login.class).bindFromRequest();
+		    if(loginForm.hasErrors()) {
+		        return badRequest(login.render(loginForm));
+		    } else {
+		        session("username", loginForm.get().username);
+		        return redirect(
+		            routes.Projects.index()
+		        );
+		    }
+		}
 
-    public static Result authenticate() {
-    	Login login = loginForm.bindFromRequest().get();
-        return ok("ログインしたユーザは " + login.username + " です");
+	 public static Boolean authenticate(String username, String password) {
+		    return find.where()
+		        .eq("username", username)
+		        .eq("password", password)
+		        .findUnique();
+		}
 
-    }
 }
