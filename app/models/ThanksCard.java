511a9ceaeedfefa8365644f5b09756ca355fda6e
iTAP <iTAP@iTAP-PC20>
2015-05-25 17:24:11 +0900

    date以外のやつの確認

--- app/models/ThanksCard.java
+++ app/models/ThanksCard.java
@@ -9,8 +9,7 @@ public class ThanksCard extends Model {
 	@Id
 	public Integer ThanksCard_ID;
 	public Integer CardMaker_ID;
-	public date Transmission_date;
-	public date Help_date;
+
 	public Integer Helpcategory_ID;
 	public String Help;
 	public String Thanks;
