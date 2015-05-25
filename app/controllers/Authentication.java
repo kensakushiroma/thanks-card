d7a3b5fe2ee28a8c41a903efdf8809a57a9c1b4d
kh030201 <k_h_030201@yahoo.co.jp>
2015-05-25 13:01:01 +0900

    Revert "login画面"

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -7,40 +7,21 @@ import views.html.authentication.*;
 
 
 public class Authentication extends Controller {
-    public static class Login {
-        public String username;
-        public String password;
 
+	 public static class Login {
+	         public String username;
+	         public String password;
+	 }
 
-        public String validate() {
-            if (authenticate(username, password)) {
-                return null;
-            }
-            return "Invalid username and password";
-        }
-
-        private Boolean authenticate(String username, String password) {
-            return (username.equals("gongo") && password.equals("pizza"));
-        }
-    }
-
-
-
-    public static Form<Login> loginForm = Form.form(Login.class);
+	 public static Form<Login> loginForm = Form.form(Login.class);
 
     public static Result index() {
-        return ok(index.render(loginForm));
+    	return ok(index.render(loginForm));
     }
 
     public static Result authenticate() {
-    	Form<Login> form = loginForm.bindFromRequest();
-
-        if (form.hasErrors()) {
-            return badRequest(index.render(form));
-        } else {
-            Login login = form.get();
-            return ok("ようこそ " + login.username + " さん!!");
-        }
+    	Login login = loginForm.bindFromRequest().get();
+        return ok("ログインしたユーザは " + login.username + " です");
 
     }
 }
