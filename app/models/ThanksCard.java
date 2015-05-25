11caea4455100b226650c7b9e2b63d2d7755affe
iTAP <iTAP@iTAP-PC20>
2015-05-25 17:27:21 +0900

    java.util.Dateをインポート

--- app/models/ThanksCard.java
+++ app/models/ThanksCard.java
@@ -3,13 +3,15 @@ package models;
 import play.db.ebean.Model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 public class ThanksCard extends Model {
 
 	@Id
 	public Integer ThanksCard_ID;
 	public Integer CardMaker_ID;
-
+	public date Transmission_date;
+	public date Help_date;
 	public Integer Helpcategory_ID;
 	public String Help;
 	public String Thanks;
