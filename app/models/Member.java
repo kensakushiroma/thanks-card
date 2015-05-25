94c3ad4f6fa4755420495035daedf80cb988ff38
iTAP <iTAP@iTAP-PC20>
2015-05-25 17:02:30 +0900

    桁が指定できなかったので戻した(笑)

--- app/models/Member.java
+++ app/models/Member.java
@@ -10,7 +10,7 @@ public class Member extends Model {
 	public Integer Member_ID;
 	public Integer Post_ID;
 	public String Member_name;
-	public String(8) Logint_password;
+	public String Logint_password;
 	public Integer Authority_ID;
 
 }
